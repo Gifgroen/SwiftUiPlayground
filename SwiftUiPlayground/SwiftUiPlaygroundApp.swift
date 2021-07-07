edcb354a4014ae6414e4470ab853060473805065
Karsten Westra <karsten@madmultimedia.nl>
2021-07-07 16:57:08 +0200

    Initial Commit

---
+++ SwiftUiPlayground/SwiftUiPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiPlaygroundApp.swift
+//  SwiftUiPlayground
+//
+//  Created by Karsten Westra on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
